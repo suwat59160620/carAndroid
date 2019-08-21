7283cf0a4bad2dc7826d4a79feed5a840f95ada5
kritinee <59160839@go.buu.ac.th>
2019-08-21 18:08:34 +0700

    first

---
+++ app/src/main/java/buu/informatics/s59160620/carparking/MainActivity.kt
@@ -0,0 +1,12 @@
+package buu.informatics.s59160620.carparking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
